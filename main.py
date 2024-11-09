812a2033bb8a087e5367ca005620c3d30c7c50b9
SneakyDeebz <sullivangavin507@gmail.com>
2024-11-09 13:45:12 -0600

    fixed get_character_count if else statement

--- main.py
+++ main.py
@@ -2,11 +2,24 @@ def main():
     Book_path = "books/Alice_in_wonderland.txt"
     text = get_book_text(Book_path)
     word_count = get_num_words(text)
+    character_count = get_character_count(text)
     print(f"Total Word Count:{word_count}")
+    print(character_count)
+        
 
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def get_character_count(text):
+    char_count = {}
+    for c in text:
+        lower_case = c.lower()
+        if lower_case in char_count:
+            char_count[lower_case] += 1
+        else:
+            char_count[lower_case] = 1
+    return char_count
 
 def get_book_text(path):
     with open(path) as f:
