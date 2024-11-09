35763f0eee0dc60e0218054db3fb38920f748e50
SneakyDeebz <sullivangavin507@gmail.com>
2024-11-09 12:35:42 -0600

    changed main.py to return number of words rather than print entire .txt file

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 def main():
-    with open("books/Alice_in_wonderland.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
+    Book_path = "books/Alice_in_wonderland.txt"
+    text = get_book_text(Book_path)
+    word_count = get_num_words(text)
+    print(f"Total Word Count:{word_count}")
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 main()
